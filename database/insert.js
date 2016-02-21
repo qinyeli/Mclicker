aadb9160478d328644c8e260075f39d29d9828e9
kenelm007 <jessepan@umich.edu>
2016-02-20 23:52:46 -0500

    file input & output

--- database/insert.js
+++ database/insert.js
@@ -3,17 +3,38 @@ var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/MClicker_db';
 
+var fs = require("fs");
+var fileName = "data.csv";
+var nameList = [];
+
+fs.exists(fileName, function(exists) {
+  if (exists) {
+    fs.stat(fileName, function(error, stats) {
+      fs.open(fileName, "r", function(error, fd) {
+        var buffer = new Buffer(stats.size);
+
+        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+          var data = buffer.toString("utf8", 0, buffer.length);
+          var rows = data.split('\n');
+          for (var i = 0; i < rows.length; i++) {
+            var col = rows[i].split(', ');
+            nameList.push({"name":col[0], "phone":col[1], "scores":[]});
+          }
+          fs.close(fd);
+        });
+      });
+    });
+  }
+});
+
 var removeall = function(db, callback) {
   db.collection('studentInfo').deleteMany({}, function(err, results){
     callback();
   });
 };
 
-var insertDocument = function(db, callback) {
-  db.collection('studentInfo').insertMany( [
-    {"name" : "shengjie", "phone" : "7348461740", "score" : []},
-    {"name" : "nobody", "phone" : "110", "score" : []}
-  ], function(err, result) {
+var insertNameList = function(db, callback) {
+  db.collection('studentInfo').insertMany(nameList, function(err, result) {
     assert.equal(err, null);
     console.log("Insert succeed.");
     callback();
@@ -23,7 +44,7 @@ var insertDocument = function(db, callback) {
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   removeall(db, function(){});
-  insertDocument(db, function() {
+  insertNameList(db, function() {
       db.close();
   });
 });
